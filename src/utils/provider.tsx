47aa3394915cfa82c147648fd8c9f3d453d1edda
lvan-ni <gnilvan@users.noreply.github.com>
2023-09-06 11:25:06 +0200

    set up getQueryClient

--- src/utils/provider.tsx
+++ src/utils/provider.tsx
@@ -1,16 +1,19 @@
+'use client';
 import React from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Providers = ({ children }: React.PropsWithChildren) => {
-    const [client] = React.useState(new QueryClient())
+  const [client] = React.useState(
+    new QueryClient({ defaultOptions: { queries: { staleTime: 5000 } } })
+  );
 
-    return (
-        <QueryClientProvider client={client}>
-            {children}
-            <ReactQueryDevtools initialIsOpen={false}/>
-        </QueryClientProvider>
-    )
-}
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
 
-export default Providers
+export default Providers;
